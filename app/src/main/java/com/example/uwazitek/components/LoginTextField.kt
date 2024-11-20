3532ca74a1c40aea9fa7ba4e5fd18a906b0667eb
unknown <patiencemugambi@gmail.com>
2024-11-20 07:06:44 +0300

    LoginTextField styling

--- app/src/main/java/com/example/uwazitek/components/LoginTextField.kt
+++ app/src/main/java/com/example/uwazitek/components/LoginTextField.kt
@@ -1,32 +1,3 @@
-//package com.example.uwazitek.components
-//
-//import androidx.compose.material.OutlinedTextField
-//import androidx.compose.material.Text
-//import androidx.compose.material.Icon
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.vector.ImageVector
-//import androidx.compose.ui.text.input.KeyboardType
-//import androidx.compose.ui.text.input.VisualTransformation
-//
-//@Composable
-//fun LoginTextField(
-//    modifier: Modifier = Modifier,
-//    value: String,
-//    onValueChange: (String) -> Unit,
-//    labelText: String,
-//    leadingIcon: ImageVector? = null,
-//    keyboardType: KeyboardType = KeyboardType.Text,
-//    visualTransformation: VisualTransformation = VisualTransformation.None
-//) {
-//    OutlinedTextField(
-//        modifier = modifier,
-//        value = value,
-//        onValueChange = onValueChange,
-//        label = { Text(labelText) },
-//
-//            )
-//}
 package com.example.uwazitek.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
